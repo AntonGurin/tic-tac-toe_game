ef516cac5857f4899f6f3dfb054782e0ba3f73a0
AntonGurin <79761911+AntonGurin@users.noreply.github.com>
2022-10-04 15:29:01 +0300

    rewrite the Square

--- src/index.js
+++ src/index.js
@@ -4,21 +4,14 @@ import './index.css';
 
 
 class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-    render() {
-      return (
-        <button
-         className="square" 
-         onClick={ ()=> this.setState({value: 'X'})}
-        >
-          {this.state.value}
-        </button>
+  render() {
+    return (
+      <button
+        className="square" 
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
       );
     }
   }
